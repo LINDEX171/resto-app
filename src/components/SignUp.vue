adea8fa8fbf0cb1f4006ae48f0b04144832cee33
LINDEX171 <lindex171@gmail.com>
2024-10-31 15:23:51 +0000

    no redirect to signin page if the user is logged in

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -41,6 +41,12 @@ export default {
 
         }
     },
+    mounted() {
+        let user = localStorage.getItem("user-info")
+        if(user){
+            this.$router.push({name:'HomePage'})  
+        }
+    },
 }
 </script>
 
