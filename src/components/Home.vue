2341abe1cc9657f2b895fcf511559ee32baed674
LINDEX171 <lindex171@gmail.com>
2024-11-01 21:08:34 +0000

    list restaurant

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,52 @@
 <template>
-   <Header/>
+  <Header />
   <h1>Welcome {{ name }} to home componets</h1>
+  <table border="1">
+    <tr>
+      <td>id</td>
+      <td>name</td>
+      <td>contact</td>
+      <td>address</td>
+    </tr>
+
+    <tr v-for="item in restaurant" :key="item.id">
+      <td>{{ item.id }}</td>
+      <td>{{ item.name }}</td>
+      <td>{{ item.contact }}</td>
+      <td>{{ item.address }}</td>
+    </tr>
+  </table>
 </template>
 <script>
-import Header from './Header.vue';
+import Header from "./Header.vue";
+import axios from "axios";
 export default {
   name: "HomePage",
-  data(){
+  data() {
     return {
-      name:''
-    }   
+      name: "",
+      restaurant: [],
+    };
   },
-  components:{Header},
-  mounted() {
+  components: {
+    Header,
+  },
+  async mounted() {
     let user = localStorage.getItem("user-info");
-    this.name = JSON.parse(user).name
+    this.name = JSON.parse(user).name;
     if (!user) {
-      this.$router.push({ name:'LoginPage'});
+      this.$router.push({ name: "LoginPage" });
     }
+    let result = await axios.get("http://localhost:3000/restaurant");
+    console.warn(result);
+    this.restaurant = result.data;
   },
 };
 </script>
+<style>
+td {
+  width: 160px;
+  height: 40px;
+  
+}
+</style>
