fe97f485f94b4295a29c504957e568dbaa04148b
LINDEX171 <lindex171@gmail.com>
2024-11-01 12:24:55 +0000

    header ui

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="nav">
+    <a href="#">Home</a>
+    <a href="#">Restaurant</a>
+    <a href="#">Update</a>
+    <a href="#">Logout</a> 
+    </div>
+</template>
+<script>
+export default {
+    name:"HeaderPage"
+}
+</script>
+<style>
+.nav{
+    background-color: #333;
+    overflow: hidden;
+}
+.nav a{
+    float: left;
+    color: #f2f2f2;
+    padding: 14px 16px;
+    text-align: center;
+    font-size: 17px;
+    text-decoration: none;
+    margin-right: 5px;
+}
+.nav a:hover{
+    background: #ddd;
+    color: #333;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,16 @@
 <template>
+   <Header/>
   <h1>Welcome to home componets</h1>
 </template>
 <script>
+import Header from './Header.vue';
 export default {
   name: "HomePage",
+  components:{Header},
   mounted() {
     let user = localStorage.getItem("user-info");
     if (!user) {
-      this.$router.push({ name:'SignUp'});
+      this.$router.push({ name:'LoginPage'});
     }
   },
 };
