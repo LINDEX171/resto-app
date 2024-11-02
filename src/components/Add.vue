f98b282e907575bc707ae926421a6093c6935983
LINDEX171 <lindex171@gmail.com>
2024-11-02 12:33:26 +0000

    add resto Ui

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,16 +1,36 @@
 <template>
-   <Header/>
-  <h1>Welcome  to Add Restaurant componets</h1>
+  <Header />
+  <h1>hello user you can add a restaurant</h1>
+  <form class="add">
+    <input type="text" name="name" v-model="restaurant.name" placeholder="Enter name" />
+    <input type="text" name="adresse" v-model="restaurant.adresse" placeholder="Enter address" />
+    <input type="text" name="contact" v-model="restaurant.contact" placeholder="Enter contact" />
+    <button type="button" v-on:click="addRestaurant">Add new restaurant</button>
+  </form>
 </template>
 <script>
-import Header from './Header.vue';
+import Header from "./Header.vue";
 export default {
   name: "AddPage",
-  components:{Header},
+  data() {
+    return {
+      restaurant :{
+        name:"",
+      adresse:"",
+      contact:""
+      }
+    }
+  },
+  methods: {
+    addRestaurant(){
+      console.warn(this.restaurant)
+    }
+  },
+  components: { Header },
   mounted() {
     let user = localStorage.getItem("user-info");
     if (!user) {
-      this.$router.push({ name:'LoginPage'});
+      this.$router.push({ name: "LoginPage" });
     }
   },
 };
